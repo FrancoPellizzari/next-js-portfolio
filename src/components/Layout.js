bf82e24e25c939b2825bc56867e77c09238985ad
Franco Paolo Pellizzari <99263736+FrancoPellizzari@users.noreply.github.com>
2024-03-15 19:07:51 +0100

    agregado error y enlace a github

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,47 @@
 import Navbar from "./Navbar.js";
 import Head from "next/head";
+import { useEffect } from "react";
+import {useRouter} from 'next/router'
+import nProgress from 'nprogress'
 
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
+  
+  const router = useRouter();
+  
+  useEffect(() =>{
+    const handleRouteChange = url => {
+      nProgress.start();
+    }
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    router.events.on('routeChangeComplete', () => nProgress.done());
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [])
+
+ return (
   <>
- 
+
     <Navbar />
-    
+
     <main className="container d-flex flex-column  py-4">
       {children}
     </main>
+    <footer className="bg-dark text-light text-center">
+      <div className="container p-4">
+        <h1>&copy; Franco Pellizzari Portfolio</h1>
+        <p>2000 - {new Date().getFullYear()}</p>
+        <p>Todos los derechos reservados.</p>
+      </div>
+    </footer>
+
   </>
-);
+ )
+  
+
+};
 
 export default Layout;
